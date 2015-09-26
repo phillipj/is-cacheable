022103a95f2aefa46c4cb1038a93dd99d34cfaab
Phillip Johnsen <johphi@gmail.com>
2015-09-26 21:16:51 +0200

    Deleted test script, wasnt supposed to be commited

--- test.js
+++
@@ -1,6 +0,0 @@
-var isCacheable = require('.')
-var request = require('request')
-
-request('http://www.vg.no', function (err, res, body) {
-  console.log('Is response from finn.com cacheable?', isCacheable(res.headers))
-})
